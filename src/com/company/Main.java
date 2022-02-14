b6b455af332db0134c94673d639500dae09ea607
Karri Wright <karriwright@Karris-MacBook-Pro.local>
2022-02-14 15:29:09 -0500

    Add initial repo

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+public class Main {
+
+    public static void main(String[] args) {
+
+        int dogWeight = 75;
+        if (dogWeight > 70) {
+        System.out.println("You have a big dog!");}
+        else if (dogWeight > 30) {
+        System.out.println("You have a medium dog!");}
+        else {
+        System.out.println("You have a small dog!");}
+    }}
